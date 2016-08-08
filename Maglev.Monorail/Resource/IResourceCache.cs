ce7debb52b0029e0a51fdc768585cefd520c78ab
JoshuaSmyth <joshuapaulsmyth@gmail.com>
2016-08-08 12:57:09 +1200

    Fixed compile error in debug mode

--- Maglev.Monorail/Resource/IResourceCache.cs
+++ Maglev.Monorail/Resource/IResourceCache.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Maglev.Monorail.Diagnostics.Logging;
 
 namespace Maglev.Monorail.Resource
 {
---
+++ pull.bat
@@ -0,0 +1,1 @@
+git pull https://github.com/JoshuaSmyth/Maglev.Monorail.git
